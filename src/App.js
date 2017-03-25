74ec12aa0a9d421e301ef1e722dc57c576313ef4
SherryH <enjoy.wind7rain@gmail.com>
2017-03-25 17:12:13 +1300

    Added basic styling to display dummy posts

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      Hello Beautiful World
+    <div id="post-list">
+      A list of posts
+      <div className="post">
+        <div className="post-user">UserId</div>
+        <div>
+          <div className="post-title">title</div>
+          <div>Message</div>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="post-user">UserId2</div>
+        <div>
+          <div className="post-title">title2</div>
+          <div>Message2</div>
+        </div>
+      </div>
+      <div className="post">
+        <div className="post-user">UserId2</div>
+        <div>
+          <div className="post-title">title2</div>
+          <div>Message2</div>
+        </div>
+      </div>
     </div>
   );
 };
