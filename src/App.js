516e6410e0ec3dbf7d369310a247d0f744a25980
SherryH <enjoy.wind7rain@gmail.com>
2017-03-26 12:56:58 +1300

    Trimmed display to 10 posts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       posts: [],
+      currentPosts: [],
+      numPosts: 10
     };
     this.getPosts = this.getPosts.bind(this);
   }
@@ -20,15 +22,16 @@ class App extends React.Component {
     .then((res) => {
       this.setState({
         posts: res.data,
+        currentPosts: res.data.slice(0,10),
       });
     });
   }
   render() {
     return (
       <div id={styles.postList}>
-        A list of posts
+        <h2>A list of posts</h2>
         {
-          this.state.posts.map(post => (
+          this.state.currentPosts.map(post => (
             <Post key={post.id.toString()} userId={post.userId} title={post.title} body={post.body} />
           ))
         }
