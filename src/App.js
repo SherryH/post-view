849c58d474a92f6f3d8886ec2e8328ed4bf50d6f
SherryH <enjoy.wind7rain@gmail.com>
2017-03-26 13:11:15 +1300

    Added the option to show more posts

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ class App extends React.Component {
     this.state = {
       posts: [],
       currentPosts: [],
-      numPosts: 10
+      numPosts: 10,
     };
     this.getPosts = this.getPosts.bind(this);
+    this.showMorePosts = this.showMorePosts.bind(this);
   }
   componentDidMount() {
     this.getPosts();
@@ -22,19 +23,29 @@ class App extends React.Component {
     .then((res) => {
       this.setState({
         posts: res.data,
-        currentPosts: res.data.slice(0,10),
+        currentPosts: res.data.slice(0, 10),
       });
     });
   }
+
+  showMorePosts() {
+    const numPosts = this.state.numPosts + 10;
+    this.setState({
+      numPosts,
+      currentPosts: this.state.posts.slice(0, numPosts),
+    });
+  }
+
   render() {
     return (
       <div id={styles.postList}>
-        <h2>A list of posts</h2>
+        <h2>A list of posts </h2>
         {
           this.state.currentPosts.map(post => (
             <Post key={post.id.toString()} userId={post.userId} title={post.title} body={post.body} />
           ))
         }
+        <button onClick={this.showMorePosts}>Show More</button>
       </div>
     );
   }
