b8b43840a981cd73a4b10003f0ba9686087235ea
SherryH <enjoy.wind7rain@gmail.com>
2017-03-28 07:40:39 +1300

    Added actions tests

--- src/__test__/actions.test.js
+++ src/__test__/actions.test.js
@@ -1,8 +1,23 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import moxios from 'moxios';
 import * as actions from '../actions';
-import { GET_POSTS_SUCCESS, SHOW_MORE_POSTS } from  '../constants';
+import { GET_POSTS_SUCCESS, SHOW_MORE_POSTS } from '../constants';
 
-describe('actions', () => {
-  xit ('should create a GET_POSTS_SUCCESS action', () => {
+describe('action creators', () => {
+
+    beforeEach( () => {
+      // import and pass your custom axios instance to this method
+      moxios.install();
+    });
+
+    afterEach( () => {
+      // import and pass your custom axios instance to this method
+      moxios.uninstall();
+    });
+
+  it ('should create a GET_POSTS_SUCCESS action when fetching has been done', () => {
+    // use nock to intercept any http request and return mock data
     const posts = [{
       id: 2,
       userId: 2,
@@ -14,10 +29,32 @@ describe('actions', () => {
       title: 'title3',
       body: 'body3',
     }];
+
+    moxios.stubRequest('https://jsonplaceholder.typicode.com/posts', {
+      status: 200,
+      data: posts,
+    });
+
+    const middlewares = [thunk]; // add your middlewares like `redux-thunk`
+    const mockStore = configureStore(middlewares);
+    const store = mockStore({ numPosts: 10, posts: [], currentPosts: [] });
     const expectedAction = {
       type: GET_POSTS_SUCCESS,
       posts,
     };
-    expect(actions.getPostsSuccess(posts)).toEqual(expectedAction);
-  })
-})
+
+    moxios.wait(() => {
+      store.dispatch(actions.getPosts())
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedAction);
+      });
+    });
+  });
+
+  it ('should create a SHOW_MORE_POSTS action', () => {
+    const expectedAction = {
+      type: SHOW_MORE_POSTS,
+    };
+    expect(actions.showMorePosts()).toEqual(expectedAction);
+  });
+});
