234d9995103ade2a4a4e23b45e2924444ec70386
SherryH <enjoy.wind7rain@gmail.com>
2017-03-31 08:41:45 +1300

    Added the explanation on css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+// Q: how are CSS extracted to be Modular CSS?
+// https://github.com/webpack-contrib/css-loader
+// style-loader, css-loader are the loaders used
+// param: moodules - specifices we gonna extract to CSS modules
+// importLoaders - how many loaders after css-loader are used e.g. postcss-loader
+// localIdentName is used for configuring the new identified name in the generated CSS. add 5 char from base64 hash [hash:base64:5]
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -16,7 +23,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' }),
+        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?modules&importLoaders=0&localIdentName=[name]__[local]___[hash:base64:5]' }),
       },
     ],
   },
